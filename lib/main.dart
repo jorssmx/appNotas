c637191dd6ed5d524b5edb0f2dd63e394ad700b4
jorssmx <jorge.minecraftyoo@gmail.com>
2025-01-31 22:22:48 -0600

    quité el DEBUG

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,  // Add this line here
       title: 'App de Notas',
       theme: ThemeData(
         primarySwatch: Colors.blue,
